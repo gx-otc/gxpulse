af805ede26c753844600fbc0e99445d526f357de
gx-otc <abdulfcfa@gmail.com>
2026-03-07 22:16:25 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const supabase = supabase.createClient(
+  "https://vglnekxeyypojwulsaqt.supabase.co",
+  "sb_publishable_4Q6O8-pl_ILuWrVW-MMqMw_ErlN7QDj"
+);
+
+// Example test query:
+console.log("Supabase client loaded:", supabase);
